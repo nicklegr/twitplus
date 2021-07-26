7388371ab543eb827954eb4fdae3d66c95b784e3
nicklegr <1327502+nicklegr@users.noreply.github.com>
2021-07-26 22:53:30 +0900

    useStateの実験

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.tsx</code> and SAVE to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
-);
+const App = () => {
+  const [tweetUrl, setTweetUrl] = useState("")
+
+  return (
+    <div className="App">
+      Tweet URL: <input className="tweet-url" type="text" onChange={(e) => setTweetUrl(e.target.value)}></input>
+      <hr />
+      <a href={tweetUrl}>{tweetUrl}</a>
+    </div>
+  )
+}
 
 export default App;
