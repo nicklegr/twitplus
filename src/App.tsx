5b72e21201db1ba2568772e611067c1143fd37bb
nicklegr <1327502+nicklegr@users.noreply.github.com>
2021-07-30 00:22:30 +0900

    ツイートが削除されてる場合の表示

--- src/App.tsx
+++ src/App.tsx
@@ -27,36 +27,56 @@ const App = () => {
 }
 
 const Tweet = ({status_id}: {status_id: string}) => {
+  const [isNotFound, setIsNotFound] = useState(false)
   const [photoUrls, setPhotoUrls] = useState(new Array<string>());
   const [tweet, setTweet] = useState<any>(null);
 
   useEffect(() => {
     const f = async () => {
       let res = await fetch(`http://localhost:8080/api/v1/photos?status_id=${status_id}`)
+      if (!res.ok) {
+        setIsNotFound(true)
+        return
+      }
       let body = await res.json()
       setPhotoUrls(body)
 
       res = await fetch(`http://localhost:8080/api/v1/tweet?status_id=${status_id}`)
+      if (!res.ok) {
+        setIsNotFound(true)
+        return
+      }
       body = await res.json()
       setTweet(body)
     }
     f()
   }, [status_id])
 
-  return (
-    <div className="tweet">
-      <hr />
-      <div className="tweet-desc">
-        <img src={tweet?.user?.profile_image_url_https} alt="" />
-        <a href={`https://twitter.com/${tweet?.user?.screen_name}`}>@{tweet?.user?.screen_name}</a>
-        <br />
-        <span>{tweet?.full_text}</span>
+  if (!isNotFound) {
+    return (
+      <div className="tweet">
+        <hr />
+        <div className="tweet-desc">
+          <img src={tweet?.user?.profile_image_url_https} alt="" />
+          <a href={`https://twitter.com/${tweet?.user?.screen_name}`}>@{tweet?.user?.screen_name}</a>
+          <span>　</span>
+          <a href={`https://twitter.com/dummy/status/${status_id}`}>{status_id}</a>
+          <br />
+          <span>{tweet?.full_text}</span>
+        </div>
+        <div className="tweet-thumbs">
+          { photoUrls.map((x, i) => <Photo url={x} tweet={tweet} index={i + 1} />) }
+        </div>
       </div>
-      <div className="tweet-thumbs">
-        { photoUrls.map((x, i) => <Photo url={x} tweet={tweet} index={i + 1} />) }
-      </div>      
-    </div>
-  )
+    )
+  } else {
+    return (
+      <div className="tweet">
+        <hr />
+        ツイートは削除されています: {status_id}
+      </div>
+    )
+  }
 }
 
 const Photo = ({url, tweet, index}: {url: string, tweet: any, index: number}) => {
